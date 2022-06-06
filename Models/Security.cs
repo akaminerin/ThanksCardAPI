16dc9aa7b93bcd0504f17c5c3235e09521706a3a
Rin <akamine.423909@occ.co.jp>
2022-06-06 17:19:28 +0900

    thankscard0606

--- Models/Security.cs
+++ Models/Security.cs
@@ -1,10 +1,11 @@
-﻿namespace ThanksCardAPI.Models
+﻿#nullable disable
+namespace ThanksCardAPI.Models
 {
     //制限テーブル
     public class Security
     {
         public long id { get; set; }
-        //public bool IsAdmin { get; set; }
-        //public string SecurityName { get; set; }
+        public bool IsAdmin { get; set; }
+        public string SecurityName { get; set; }
     }
 }
