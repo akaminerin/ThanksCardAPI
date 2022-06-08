cd4c8c79087edd26c8eada3b9b461f2a5f2cab13
jun419u <103544861+jun419u@users.noreply.github.com>
2022-06-08 17:58:17 +0900

    Revert "thankscard0606"

--- Models/Security.cs
+++ Models/Security.cs
@@ -1,11 +1,10 @@
-﻿#nullable disable
-namespace ThanksCardAPI.Models
+﻿namespace ThanksCardAPI.Models
 {
     //制限テーブル
     public class Security
     {
         public long id { get; set; }
-        public bool IsAdmin { get; set; }
-        public string SecurityName { get; set; }
+        //public bool IsAdmin { get; set; }
+        //public string SecurityName { get; set; }
     }
 }
